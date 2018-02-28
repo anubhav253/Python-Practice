95b0886b60d706bc6465652310199fce949398b8
anubhav253 <anubhav253kumar@gmail.com>
2018-02-28 13:50:52 +0530

    Mean and median of random function.

--- np_plt.py
+++ np_plt.py
@@ -5,6 +5,8 @@ This is a temporary script file.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 x = np.array([[1,2,3], [4,5,6]], np.int32)
 print type(x)
 print x.shape
@@ -16,18 +18,10 @@ print z.dtype
 
 a = np.arange(5)
 
-
-
-import matplotlib.pyplot as plt
-
 xs = [1,2,3,4,5,6]
 ys = [1,4,9,16,25,36]
 
 plt.plot(xs, ys)
-
-
-
-import matplotlib.pyplot as plt
 
 xs = [1,2,3,4,5,6]
 ys = [x**2 for x in xs]
@@ -36,6 +30,16 @@ plt.plot(xs,ys)
 plt.xlabel("x-axis")
 plt.xlabel("y-axis")
 plt.title("Matplotlib Example")
-plt.savefig(quad.png)
+plt.savefig('quad2.png')
 plt.show()
 
+
+incomes = np.random.normal(10000, 2000, 10000)
+print incomes
+
+plt.hist(incomes, 50)
+mean = np.mean(incomes)
+median = np.median(incomes)
+
+incomes = np.append(incomes, [60000000000000000000])
+print mean
