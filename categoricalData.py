cd873e71464493fb4b6c6d3111a2cbc7f1d6abf2
anubhav253 <anubhav253kumar@gmail.com>
2018-03-13 19:53:17 +0530

    Categorical data handling using label encoder and oneHotEncoder

---
+++ categoricalData.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 17:04:48 2018
+
+@author: DEVIL
+"""
+import pandas as pd
+
+data = pd.read_csv('Cricket_2017.csv')
+features = data.iloc[:,:-1].values
+labels = data.iloc[:, -1].values
+
+#Encoding
+
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+lb = LabelEncoder()
+features[:, 0] = lb.fit_transform(features[:, 0])
+features[:, 1] = lb.fit_transform(features[:, 1])
+
+#for labeled data
+onehotencoder = OneHotEncoder(categorical_features = [0, 1])
+features = onehotencoder.fit_transform(features).toarray()
+
+labels = lb.fit_transform(labels)
