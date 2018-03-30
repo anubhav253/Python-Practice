ebd706d19071c38cc9af41d160c5eb727bf07e0f
anubhav253 <anubhav253kumar@gmail.com>
2018-03-31 04:28:38 +0530

    Reaading Foodtruck.csv in python file using pandas

---
+++ LR_foodTruck.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 31 04:22:06 2018
+
+@author: DEVIL
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#reading csv file
+dataset = pd.read_csv("Foodtruck.csv")
