ef5fb51752a71e1836bdfc837d241f2efff02c01
anubhav253 <anubhav253kumar@gmail.com>
2018-03-31 04:29:54 +0530

    Splitting of dataset performed.

--- LR_foodTruck.py
+++ LR_foodTruck.py
@@ -11,3 +11,9 @@ import matplotlib.pyplot as plt
 
 #reading csv file
 dataset = pd.read_csv("Foodtruck.csv")
+#defining dependent and independent variables
+features = dataset.iloc[:,:-1].values
+labels = dataset.iloc[:,-1].values
+#splitting the dataset
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels, test_size=0.2, random_state=0)
