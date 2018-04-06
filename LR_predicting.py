1d356000e294948f30ba7e2ed4be9d51c6d72740
anubhav253 <anubhav253kumar@gmail.com>
2018-04-06 09:42:13 +0530

    Adding new data set for linear regression.

---
+++ LR_predicting.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 06 09:38:22 2018
+
+@author: DEVIL
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Bahubali2_vs_Dangal.csv')
