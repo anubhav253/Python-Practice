86bb31442a5614b2b5a249ee87f1e30cf7b2db36
anubhav253 <anubhav253kumar@gmail.com>
2018-04-06 09:53:40 +0530

    Predicting collection on 10th days and model score.

--- LR_predicting.py
+++ LR_predicting.py
@@ -8,7 +8,7 @@ Created on Fri Apr 06 09:38:22 2018
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#taking values from csv file
+#taking values from csv file which is list of 2 movies for their starting 9days money collection
 dataset = pd.read_csv('Bahubali2_vs_Dangal.csv')
 #defining dependent and independent variables
 features = dataset.iloc[:, 0:1].values
@@ -21,3 +21,11 @@ regressor = LinearRegression()
 regressor.fit(features, labels_bahu)
 regressor2 = LinearRegression()
 regressor2.fit(features, labels_dangal)
+
+#predicting collection on 10th day
+Bahu_predict = regressor.predict(10)
+Dangal_predict = regressor2.predict(10)
+
+#model score
+Score_bahu = regressor.score(features,labels_bahu)
+Score_dangal = regressor2.score(features,labels_dangal)
