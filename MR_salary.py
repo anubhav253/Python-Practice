db286b44202dbe59274cee7fb3cf7c7fa9b2aab4
anubhav253 <anubhav253kumar@gmail.com>
2018-04-06 07:08:59 +0530

    Encoding of categorical data.

--- MR_salary.py
+++ MR_salary.py
@@ -12,3 +12,17 @@ import numpy as np
 
 #taking values from csv file which is salary classifications of employees
 dataset = pd.read_csv('Salary_Classification.csv')
+features = dataset.iloc[:, :-1].values
+labels = dataset.iloc[:,-1:].values
+
+#encoding of categoricaldata
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+l_enc = LabelEncoder()
+features[:, 0] = l_enc.fit_transform(features[:, 0])
+
+oh_enc = OneHotEncoder(categorical_features = [0])
+features = oh_enc.fit_transform(features).toarray()
+
+#avoiding dummy variable
+features = features[:, 1:]
+
