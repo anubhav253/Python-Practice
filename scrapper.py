c05591ab81eca6476c997000bcfd2e36f4418bca
anubhav253 <anubhav253kumar@gmail.com>
2018-04-07 00:17:25 +0530

    Fetching data and storing columns in lists.

--- scrapper.py
+++ scrapper.py
@@ -15,3 +15,20 @@ page = urllib2.urlopen(url)
 #using beautifulsoup to fetching data
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(page)
+#finding table in url
+right = soup.find('table', class_ = 'table')
+#empty list to store data in table
+A = []
+B = []
+C = []
+D = []
+E = []
+
+for row in right.findAll('tr'):
+    cells = row.findAll('td')
+    if len(cells) == 5:
+        A.append(cells[0].find(text=True))
+        B.append(cells[1].text.strip())
+        C.append(cells[2].find(text=True))
+        D.append(cells[3].find(text=True))
+        E.append(cells[4].find(text=True))
