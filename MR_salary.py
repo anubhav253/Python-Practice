3a2e86ee4ae6c66c59bc18f913b0835af17952b5
anubhav253 <anubhav253kumar@gmail.com>
2018-04-06 10:17:06 +0530

    Multiple linear regression for salary dataset.

---
+++ MR_salary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 06 10:12:44 2018
+
+@author: DEVIL
+"""
+
+#Multiple linear regression
+
+import pandas as pd
+import numpy as np
+
+#taking values from csv file which is salary classifications of employees
+dataset = pd.read_csv('Salary_Classification.csv')
