1f41d14a59e58f47b6fdc39fde611c2dce281ac7
anubhav253 <anubhav253kumar@gmail.com>
2018-04-26 18:44:31 +0530

    No.of clusters required using elbow method.

--- kmeans_speed_analysis.py
+++ kmeans_speed_analysis.py
@@ -11,3 +11,18 @@ import numpy as np
 #reading the dataset
 data = pd.read_csv('speeding_features.csv')
 features = data.iloc[:, [1,2]].values
+#classify data using KMeans unsupervised learning
+from sklearn.cluster import KMeans
+#taking an empty list
+wcss = []
+#elbow method to find the number of clusters required
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
+    kmeans.fit(features)
+    wcss.append(kmeans.inertia_)
+    
+plt.plot(range(1,11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
