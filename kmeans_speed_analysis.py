678852b9adf1bb2d0c9a86ffac7a191f98886021
anubhav253 <anubhav253kumar@gmail.com>
2018-04-26 18:50:48 +0530

    2 clusters are used and plotting the graph for prediction.

--- kmeans_speed_analysis.py
+++ kmeans_speed_analysis.py
@@ -20,9 +20,21 @@ for i in range(1, 11):
     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
     kmeans.fit(features)
     wcss.append(kmeans.inertia_)
-    
+#plotting the graph and checking where is the maximum deflection    
 plt.plot(range(1,11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
+plt.show()
+
+#2 clusters are required for this model
+kmeans = KMeans(n_clusters = 2, init = 'k-means++', random_state = 0)
+y_means = kmeans.fit_predict(features)
+#if mean percentage of time a driver was >5 mph over the speed limit then it is driving in Urban area.
+plt.scatter(features[y_means == 0, 0], features[y_means == 0,1], s = 100, c = 'red', label = 'Rural')
+plt.scatter(features[y_means == 1, 0], features[y_means == 1,1], s = 100, c = 'blue', label = 'Urban')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'green', label = 'Centroids')
+plt.xlabel('Distance')
+plt.ylabel('Speed')
+plt.legend()
 plt.show()
