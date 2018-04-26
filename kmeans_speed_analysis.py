8c64f000dba3e1a14786601b373c288fa569843f
anubhav253 <anubhav253kumar@gmail.com>
2018-04-26 18:36:52 +0530

    Reading the csv data and taking the features variable.

--- kmeans_speed_analysis.py
+++ kmeans_speed_analysis.py
@@ -5,3 +5,9 @@ Created on Thu Apr 26 18:30:09 2018
 @author: DEVIL
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+#reading the dataset
+data = pd.read_csv('speeding_features.csv')
+features = data.iloc[:, [1,2]].values
